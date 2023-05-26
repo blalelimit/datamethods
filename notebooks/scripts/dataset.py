76f65fa4ef7b82c1e55943f11553ceb0f432db50
Blale Limit <120578238+BlaleLimit@users.noreply.github.com>
2023-05-26 12:27:25 +0800

    commit

--- notebooks/scripts/dataset.py
+++ notebooks/scripts/dataset.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import random as rd
 
 
 class Food:
@@ -16,13 +15,13 @@ class Food:
 
 
     def choose_items(self):
-        n_items = rd.randint(1, self.max_items)
+        n_items = np.random.randint(1, self.max_items)
         return np.random.choice(a=self.items, size=n_items, replace=False, p=self.scaled_probability)
 
 
 def generate_arr(items, probability, max_items=5, length=10, mode='defined'):
     if mode == 'rand':
-        probability = [rd.random() for _ in range(len(items))]
+        probability = [np.random.uniform() for _ in range(len(items))]
     list_items = list()
     for _ in range(length):
         chosen_items = Food(items, probability, max_items).choose_items()
